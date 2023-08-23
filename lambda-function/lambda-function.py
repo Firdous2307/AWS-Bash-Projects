beb0feb16cbf9f297d28dea73b3b0e6a9be756c1
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-08-23 23:10:30 +0000

    work in progress

--- lambda-function/lambda-function.py
+++ lambda-function/lambda-function.py
@@ -5,7 +5,18 @@ import boto3
 dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    # Get HTTP method from the API Gateway event
-    http_method = event['httpMethod']
-    
-  
+    try:
+        http_method = event['httpMethod']
+        table_name = "$TABLE_NAME"
+        
+        if http_method == 'GET':
+            response = dynamodb.scan(TableName="$TABLE_NAME")
+            items = response.get('Items', [])
+            
+            return {
+                "statusCode": 200,
+                "body": json.dumps(items)
+            }
+        elif http_method == 'POST':
+            request_body = json.loads(event['body'])
+            
