3c0c3435ec535bc5e7a78f6c6388a1e1f4b85727
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-08-23 23:27:50 +0000

    zipped lambda function

--- lambda-function/lambda-function.py
+++ lambda-function/lambda-function.py
@@ -7,10 +7,10 @@ dynamodb = boto3.client('dynamodb')
 def lambda_handler(event, context):
     try:
         http_method = event['httpMethod']
-        table_name = "$TABLE_NAME"
+        table_name = ToDOApp
         
         if http_method == 'GET':
-            response = dynamodb.scan(TableName="$TABLE_NAME")
+            response = dynamodb.scan(TableName=ToDOApp)
             items = response.get('Items', [])
             
             return {
@@ -20,3 +20,30 @@ def lambda_handler(event, context):
         elif http_method == 'POST':
             request_body = json.loads(event['body'])
             
+            if 'task' not in request_body:
+                return {
+                    "statusCode": 400,
+                    "body": "Missing 'task' field in request"
+                }
+            
+            new_item = {
+                'id': {'S': str(hash(request_body['task']))},
+                'task': {'S': request_body['task']}
+            }
+            
+            dynamodb.put_item(TableName=ToDOApp, Item=new_item)
+            
+            return {
+                "statusCode": 201,
+                "body": "Item added successfully"
+            }
+        else:
+            return {
+                "statusCode": 405,
+                "body": "Method not allowed"
+            }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": f"An error occurred: {str(e)}"
+        }
