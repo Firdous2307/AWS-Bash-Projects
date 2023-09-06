9d4f5bcc53d5be716510e3e14eaf21cc3c799682
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-09-06 18:47:20 +0000

    working on testing API

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,5 @@
 // Function to submit a POST request to add a task
-function submitForm() {
+function submitForm(event) {
     event.preventDefault();
 
     // Get form data
@@ -11,7 +11,7 @@ function submitForm() {
     const xhr = new XMLHttpRequest();
 
     // Set up request
-    xhr.open('POST', 'API_INVOKE_URL/addTask', true);
+    xhr.open('POST', 'https://fczp9ttbpb.execute-api.us-east-2.amazonaws.com/prod/addTask', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     // Set up response handler
@@ -21,7 +21,7 @@ function submitForm() {
                 alert('Task added successfully!');
                 document.getElementById('task').value = '';
                 document.getElementById('deadline').value = '';
-                document.getElementById('priority').value = 'high'; // Reset priority to default
+                document.getElementById('priority').value = ''; 
             } else {
                 alert('Task addition failed: ' + xhr.responseText);
             }
@@ -34,86 +34,4 @@ function submitForm() {
         deadline: deadline,
         priority: priority
     }));
-}
-
-// Function to retrieve tasks via GET request
-function getTasks() {
-    // Create request object
-    const xhr = new XMLHttpRequest();
-
-    // Set up request
-    xhr.open('GET', 'API_INVOKE_URL/getTasks', true);
-
-    // Set up response handler
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                const tasks = JSON.parse(xhr.responseText);
-                // Handle the retrieved tasks (e.g., display them on the page)
-                console.log('Retrieved tasks:', tasks);
-            } else {
-                alert('Failed to retrieve tasks: ' + xhr.responseText);
-            }
-        }
-    };
-
-    // Send request
-    xhr.send();
-}
-
-// Function to update a task via PATCH request
-function updateTask() {
-    // Get task data to update
-    const taskToUpdate = 'task_name'; // Replace with the task name you want to update
-    const newDeadline = 'new_deadline'; // Replace with the new deadline
-    const newPriority = 'new_priority'; // Replace with the new priority
-
-    // Create request object
-    const xhr = new XMLHttpRequest();
-
-    // Set up request
-    xhr.open('PATCH', `API_INVOKE_URL/updateTask/${taskToUpdate}`, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-
-    // Set up response handler
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                alert('Task updated successfully!');
-            } else {
-                alert('Failed to update task: ' + xhr.responseText);
-            }
-        }
-    };
-
-    // Send request
-    xhr.send(JSON.stringify({
-        deadline: newDeadline,
-        priority: newPriority
-    }));
-}
-
-// Function to delete a task via DELETE request
-function deleteTask() {
-    // Get task name to delete
-    const taskToDelete = 'task_name'; 
-    // Create request object
-    const xhr = new XMLHttpRequest();
-
-    // Set up request
-    xhr.open('DELETE', `API_INVOKE_URL/deleteTask/${taskToDelete}`, true);
-
-    // Set up response handler
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                alert('Task deleted successfully!');
-            } else {
-                alert('Failed to delete task: ' + xhr.responseText);
-            }
-        }
-    };
-
-    // Send request
-    xhr.send();
 }
--- lambda_function/lambda.py
+++ lambda_function/lambda.py
@@ -3,16 +3,16 @@ import boto3
 
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('ToDoApp')  
+table = dynamodb.Table('ToDoList')  
 
 def lambda_handler(event, context):
-    if event['httpMethod'] == 'POST':
+        print(event)
         # Create new item in DynamoDB table
         response = table.put_item(
             Item={
                 'task': event['task'],
                 'deadline': event['deadline'],
-                'priority': event['priority'],
+                'priority': event['priority']
             }
         )
         
@@ -25,93 +25,4 @@ def lambda_handler(event, context):
             },
             'body': json.dumps({'message': 'Task successfully added'})
         }
-    elif event['httpMethod'] == 'GET':
-        # Retrieve all items from DynamoDB table
-        response = table.scan()
         
-        items = response.get('Items', [])
-        
-        # Return response
-        return {
-            'statusCode': 200,
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body': json.dumps(items)
-        }
-    elif event['httpMethod'] == 'PATCH':
-        # Update an item in the DynamoDB table based on task name
-        task_name = event['queryStringParameters']['task'] if 'queryStringParameters' in event else None
-
-        if task_name:
-            response = table.update_item(
-                Key={
-                    'task': task_name
-                },
-                UpdateExpression='SET deadline = :d, priority = :p',
-                ExpressionAttributeValues={
-                    ':d': event['deadline'],
-                    ':p': event['priority']
-                }
-            )
-
-            # Return response
-            return {
-                'statusCode': 200,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                'body': json.dumps({'message': 'Task successfully updated'})
-            }
-        else:
-            # Invalid or missing task parameter
-            return {
-                'statusCode': 400,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                'body': json.dumps({'message': 'Invalid or missing task parameter'})
-            }
-    elif event['httpMethod'] == 'DELETE':
-        # Delete an item from the DynamoDB table based on task name
-        task_name = event['queryStringParameters']['task'] if 'queryStringParameters' in event else None
-
-        if task_name:
-            response = table.delete_item(
-                Key={
-                    'task': task_name
-                }
-            )
-
-            # Return response
-            return {
-                'statusCode': 200,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                'body': json.dumps({'message': 'Task successfully deleted'})
-            }
-        else:
-            # Invalid or missing task parameter
-            return {
-                'statusCode': 400,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                'body': json.dumps({'message': 'Invalid or missing task parameter'})
-            }
-    else:
-        # Handle other HTTP methods or return an error if needed
-        return {
-            'statusCode': 405,
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body': json.dumps({'message': 'Method not allowed'})
-        }
